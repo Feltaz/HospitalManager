2f1803d0dfb242992fa13d5d89693e1f03ded80e
feltaz <ouerfelli.moetaz@gmail.com>
2022-03-23 20:54:35 +0100

    project added

---
+++ src/main/java/tn/enicar/groupb/hospitalmanager/HospitalManagerApplication.java
@@ -0,0 +1,13 @@
+package tn.enicar.groupb.hospitalmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/enicar/groupb/hospitalmanager/HospitalManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.enicar.groupb.hospitalmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

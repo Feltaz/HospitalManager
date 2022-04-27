53f278f39d62b5b551701862c17c24e8a54989ad
feltaz <ouerfelli.moetaz@gmail.com>
2022-04-27 15:36:46 +0100

    added logging to the serviceService and serviceController

--- src/main/java/tn/enicar/groupb/hospitalmanager/Service/ServiceController.java
+++ src/main/java/tn/enicar/groupb/hospitalmanager/Service/ServiceController.java
@@ -1,5 +1,6 @@
 package tn.enicar.groupb.hospitalmanager.Service;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import tn.enicar.groupb.hospitalmanager.Patient.Patient;
@@ -9,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path="/api/service")
+@Log4j2
 public class ServiceController {
 private final ServiceService serviceService;
 
@@ -29,18 +31,23 @@ private final ServiceService serviceService;
     public void addService(@RequestBody  Service service){
 
     serviceService.addService(service);
+    log.info(String.format("Service %s added",service.getNom()));
     }
     @DeleteMapping(path="/{id}")
     public void deleteService(@PathVariable("id") long id){
         serviceService.deleteService(id);
+        log.info(String.format("Service %s deleted",id));
     }
     @PutMapping(path="/{id}")
     public void updateService(@RequestBody Service service, @PathVariable("id") long id){
         serviceService.updateService(service,id);
+        log.info(String.format("Service %s updated",service.getNom()));
     }
     @GetMapping(path="/{id}")
     public Service getService(@PathVariable("id") long id){
+
         return serviceService.getService(id);
+
     }
 
 
--- src/main/java/tn/enicar/groupb/hospitalmanager/Service/ServiceService.java
+++ src/main/java/tn/enicar/groupb/hospitalmanager/Service/ServiceService.java
@@ -1,10 +1,12 @@
 package tn.enicar.groupb.hospitalmanager.Service;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 @org.springframework.stereotype.Service
+@Log4j2
 public class ServiceService {
     private final ServiceRepository serviceRepository;
 
@@ -14,26 +16,47 @@ public class ServiceService {
     }
 
     public List<Service> getServices(){
-
+        boolean empty= serviceRepository.findAll().isEmpty();
+        if(empty) {
+            log.warn("No services found");
+            log.warn("attemtpted to get empty list of services");
+            throw new IllegalStateException("No services found");
+        }
+        else{
+            log.info("Services found and retrieved");
         return
-                serviceRepository.findAll();
+                serviceRepository.findAll();}
     }
 
     public void addService(Service service) {
         boolean exists= serviceRepository.existsById(service.getId());
-        if(exists) throw new IllegalStateException("Service already exists");
+        log.info("Attempting to add service with id: " + service.getId());
+        if(exists) {
+            log.warn("Service already exists");
+            throw new IllegalStateException("Service already exists");
+
+        }
         serviceRepository.save(service);
+        log.info("Service added");
     }
 
     public void deleteService(long id) {
         boolean exists = serviceRepository.existsById(id);
-        if(!exists) throw new IllegalStateException("Service does not exist");
+        log.info("Attempting to delete service with id: " + id);
+        if(!exists) {
+            log.warn(String.format("Service with id %s does not exist"),id);
+            throw new IllegalStateException("Service does not exist");
+        }
         serviceRepository.deleteById(id);
+        log.info(String.format("Service with id %s deleted",id));
     }
 
     public void updateService(Service service, long id) {
         boolean exists = serviceRepository.existsById(id);
-        if(!exists) throw new IllegalStateException("Service does not exist");
+        log.info("Attempting to update service with id: " + id);
+        if(!exists) {
+            log.warn(String.format("Service with id %s does not exist"),id);
+            throw new IllegalStateException("Service does not exist");}
         serviceRepository.deleteById(id);
         service.setId(id);
         service.setNom(service.getNom());
@@ -43,7 +66,13 @@ public class ServiceService {
 
     public Service getService(long id) {
         boolean exists = serviceRepository.existsById(id);
-        if(!exists) throw new IllegalStateException("Service does not exist");
+        log.info("Attempting to fetch service with id: " + id);
+        if(!exists) {
+            log.warn(String.format("Service with id %s does not exist"),id);
+            throw new IllegalStateException("Service does not exist");
+        }
+        log.info(String.format("Service %s found",id));
+        log.info(String.format("Service %s fetched",id));
         return serviceRepository.findById(id).get();
     }
 }
